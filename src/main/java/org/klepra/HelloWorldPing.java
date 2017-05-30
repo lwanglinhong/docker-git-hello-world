69efee511bd8e11a6615fd11a9b66a3b32bd98fe
klepra <klemen.pravdic@gmail.com>
2017-05-30 23:30:39 +0200

    readme

---
+++ src/main/java/org/klepra/HelloWorldPing.java
@@ -0,0 +1,11 @@
+package org.klepra;
+
+public class HelloWorldPing {
+	
+	public static void main(String args[]) throws Exception{
+		for(int i = 0 ; i < 500; i++){
+			System.out.println("Hello World Ping " + i );
+			Thread.sleep(1000);
+		}
+	}
+}
