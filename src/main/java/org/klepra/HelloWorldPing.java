f0519b027355990af0537035e0698d913a801edd
klepra <klemen.pravdic@gmail.com>
2017-05-31 00:10:18 +0200

    fix

--- src/main/java/org/klepra/HelloWorldPing.java
+++ src/main/java/org/klepra/HelloWorldPing.java
@@ -4,7 +4,7 @@ public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
 		for(int i = 0 ; i < 500; i++){
-			System.out.println("Hello World Ping " + i );
+			System.out.println("Hello World " + i );
 			Thread.sleep(1000);
 		}
 	}
